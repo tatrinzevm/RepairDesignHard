d3e1178bc46a115512fd80e797bc716e642ae1eb
tatrinzevm <tatrinzevm@gmail.com>
2020-03-03 00:16:42 +0200

    Clean-css and rename has been included. Mincss has been maked. Is it working.

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
 
-gulp.task('hello', function(done) {
-  console.log('Hello');
-  done();
+gulp.task('mincss', function() {
+  return gulp.src("css/*.css")
+  .pipe(rename({suffix: ".min"}))
+  .pipe(cleanCSS())
+  .pipe(gulp.dest("css"));
 });
 
-gulp.task('browser-sync', function() {
-  browserSync.init({
-      server: {
-          baseDir: "./"
-      }
-  });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
